2c734937319f96f5f661e7463defffbb6d0073af
Ryan Ojeda <ryan.ojeda@disney.com>
2021-01-28 15:14:12 -0800

    ryan exercise 9-1

---
+++ students/ryan/exercise_9_1.py
@@ -0,0 +1,8 @@
+my_list = ['Ryan', 'Dave', 'John', 'Jake']
+my_list.append('Dash')
+print('People in my game:')
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(my_list[3])
+print(my_list[4])
