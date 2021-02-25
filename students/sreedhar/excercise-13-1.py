893373cec598c34666f2c068e8f21f2a1ef260f8
Konireddy <Sreedhar.Konireddy@disney.com>
2021-02-25 16:29:32 -0500

    solution for ex13-1

---
+++ students/sreedhar/excercise-13-1.py
@@ -0,0 +1,10 @@
+str1 = 'Hello World'
+slice1 = str1[:5]
+slice2= str1[6:]
+slice3= slice1[::-1]+' '+slice2[::-1]
+print(slice1)
+print(slice2)
+print(slice3)
+
+list1 = ["a", "b", "c", "d", "e", "f", "g", "h","i"]
+print(list1[2:7:2])
