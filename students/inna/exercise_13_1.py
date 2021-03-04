f1436f393e3de96cb6da5f978e4a37496cd28905
penechek <77467437+penechek@users.noreply.github.com>
2021-03-04 17:36:20 -0500

    exercise 13-1

---
+++ students/inna/exercise_13_1.py
@@ -0,0 +1,6 @@
+hello_world = "Hello World"
+print(hello_world[0:5])
+print(hello_world[6:])
+print(hello_world[::-1])
+ABC_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+print(ABC_list[2:7])
